6c439fa09aa2044580ee33e4d8374a67c1e1d53b
CatalinStratulat <catalin.stratulat321@gmail.com>
2023-03-22 10:10:15 +0000

    edit change

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import logging
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    logging.info('')
+    logging.info('\tFirst original logging just to print')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
