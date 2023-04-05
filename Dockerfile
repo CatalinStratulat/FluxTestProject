7f94bb380fc6ad72ecbacea57a5b4d3896bb8481
CatalinStratulat <catalin.stratulat321@gmail.com>
2023-04-05 10:21:17 +0100

    working sample app of server

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM python:3.10-slim-buster
 
-LABEL descripton="Catalin Flux Test App"
+LABEL descripton="SWLCM - Gitops Git Service"
+#LABEL version="0.1.0"
 
-RUN mkdir /appPath
-COPY src/*.py /appPath/
+RUN mkdir /gitsvc
+COPY src/*.py /gitsvc/
 
-RUN apt-get update && apt-get install -y git procps
+COPY requirements.txt /tmp/
+
+RUN apt-get update && apt-get install -y git procps && \
+  pip install --upgrade pip && pip install --no-cache-dir -r /tmp/requirements.txt
+
 
 WORKDIR /gitsvc
+#CMD ["uvicorn", "gitops_adapter:app","--host", "0.0.0.0", "--port", "80"]
--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
-import logging
-# This is a sample Python script.
+from typing import Union
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    logging.info('\tFirst original logging just to print')
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
-# Press the green button in the gutter to run the script.
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uvicorn.run(app, host='0.0.0.0', port=8080)
